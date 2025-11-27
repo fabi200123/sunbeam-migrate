2f79215d534d4f0e1fc60b1775248380ee3604b3
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-11-27 12:36:06 +0200

    Add dependency for router

--- sunbeam_migrate/handlers/neutron/router.py
+++ sunbeam_migrate/handlers/neutron/router.py
@@ -25,7 +25,20 @@ class RouterHandler(base.BaseMigrationHandler):
 
         Associated resources must be migrated first.
         """
-        return []
+        return ["network"]
+
+    def get_associated_resources(self, resource_id):
+        """Return the source resources this router depends on."""
+        source_router = self._source_session.network.get_router(resource_id)
+        if not source_router:
+            raise exception.NotFound(f"Router not found: {resource_id}")
+
+        associated_resources = []
+        for network_ref in [source_router.external_gateway_info.get("network_id")]:
+            if network_ref:
+                associated_resources.append(("network", network_ref))
+
+        return associated_resources
 
     def get_implementation_status(self) -> str:
         """Describe the implementation status."""
@@ -56,6 +69,13 @@ class RouterHandler(base.BaseMigrationHandler):
         if not source_router:
             raise exception.NotFound(f"Router not found: {resource_id}")
 
+        external_gateway_network_id = None
+        if source_router.external_gateway_info:
+            external_gateway_network_id = self._get_associated_resource_destination_id(
+                "network",
+                source_router.external_gateway_info.get("network_id"),
+                migrated_associated_resources,
+            )
         fields = [
             "availability_zone_hints",
             "description",
@@ -72,6 +92,8 @@ class RouterHandler(base.BaseMigrationHandler):
             value = getattr(source_router, field, None)
             if value is not None:
                 kwargs[field] = value
+                if field == "external_gateway_info" and external_gateway_network_id:
+                    kwargs[field]["network_id"] = external_gateway_network_id
         
         destination_router = self._destination_session.network.create_router(**kwargs)
         return destination_router.id
